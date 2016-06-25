05ef5995cf7d631f8125e4853ecb5ce57baccb1c
Edward Moseley <etmoseley@yahoo.com>
2016-06-25 19:35:15 -0400

    Servo Motor

--- Arduino/sketch_jun13a/sketch_jun13a.ino
+++ Arduino/sketch_jun13a/sketch_jun13a.ino
@@ -1,9 +1,14 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Servo.h> //load servo library
 
+Servo myServo; //initialize myServo object
+
+void setup() {
+  myServo.attach(13);//attach servo controller to pin 13
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  myServo.write(0);//position motor at 0 degrees
+  delay(1000); //wait 1000milliseconds
+  myServo.write(180);//position motor to 180 degrees
+  delay(3000);//wait 3 seconds
 }
